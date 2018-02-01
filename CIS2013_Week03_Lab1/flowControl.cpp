1957685e2fa396090fb64fc1118bcf4b43dc566e
kevin115533 <35547586+kevin115533@users.noreply.github.com>
2018-01-31 20:13:37 -0600

    Lab 1

--- CIS2013_Week03_Lab1/flowControl.cpp
+++ CIS2013_Week03_Lab1/flowControl.cpp
@@ -2,5 +2,23 @@
 using namespace std;
 
 int main(){
+	char continueProgram = 'x';
 
+	do {
+
+		cout << "Do you want to continue? (y/n)" << endl;
+		cin >> continueProgram;
+
+		switch (continueProgram) {
+		case 'y':
+			cout << "Going to continue" << endl;
+			break;
+		case 'n':
+			cout << "Going to exit" << endl;
+			break;
+		default:
+			continueProgram = 'y';
+		}
+	} while (continueProgram == 'y');
+	return 0;
 }
